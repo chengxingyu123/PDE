5382747670efce77518b693b7724fd1ff9cf2271
wmelhem <wmelhem>
2004-10-01 09:15:32 +0000

    *** empty log message ***

--- org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/launcher/WorkbenchLaunchConfigurationDelegate.java
+++ org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/launcher/WorkbenchLaunchConfigurationDelegate.java
@@ -52,7 +52,7 @@ public class WorkbenchLaunchConfigurationDelegate extends LaunchConfigurationDel
 			}
 
 			// clear config area, if necessary
-			if (configuration.getAttribute(CONFIG_CLEAR, false))
+			if (configuration.getAttribute(CONFIG_CLEAR_AREA, false))
 				LauncherUtils.clearConfigArea(getConfigDir(configuration), new SubProgressMonitor(monitor, 1));
 			launch.setAttribute(ILauncherSettings.CONFIG_LOCATION, getConfigDir(configuration).toString());
 			
@@ -136,12 +136,15 @@ public class WorkbenchLaunchConfigurationDelegate extends LaunchConfigurationDel
 			if (pluginMap == null) 
 				return null;
 				
-			String primaryFeatureId = LauncherUtils.getBrandingPluginID(configuration);
+			String brandingPlugin = LauncherUtils.getBrandingPluginID(configuration);
+			if (isOSGI) {
+				LauncherUtils.createConfigIniFile(configuration,
+						brandingPlugin, pluginMap, getConfigDir(configuration));
+			}
 			TargetPlatform.createPlatformConfigurationArea(
 					pluginMap,
 					getConfigDir(configuration),
-					primaryFeatureId,
-					LauncherUtils.getAutoStartPlugins(configuration));
+					brandingPlugin);
 			programArgs.add("-configuration"); //$NON-NLS-1$
 			if (isOSGI)
 				programArgs.add("file:" + new Path(getConfigDir(configuration).getPath()).addTrailingSeparator().toString()); //$NON-NLS-1$
@@ -149,9 +152,9 @@ public class WorkbenchLaunchConfigurationDelegate extends LaunchConfigurationDel
 				programArgs.add("file:" + new Path(getConfigDir(configuration).getPath()).append("platform.cfg").toString()); //$NON-NLS-1$ //$NON-NLS-2$
 			
 			if (!isOSGI) {
-				if (primaryFeatureId != null) {
+				if (brandingPlugin != null) {
 					programArgs.add("-feature"); //$NON-NLS-1$
-					programArgs.add(primaryFeatureId);					
+					programArgs.add(brandingPlugin);					
 				}
 				IPluginModelBase bootModel = (IPluginModelBase)pluginMap.get("org.eclipse.core.boot"); //$NON-NLS-1$
 				String bootPath = LauncherUtils.getBootPath(bootModel);
@@ -302,16 +305,16 @@ public class WorkbenchLaunchConfigurationDelegate extends LaunchConfigurationDel
 	private File getConfigDir(ILaunchConfiguration config) {
 		if (fConfigDir == null) {
 			try {
-				if (config.getAttribute(USEFEATURES, false)) {
+				if (config.getAttribute(USEFEATURES, false) && config.getAttribute(CONFIG_USE_DEFAULT_AREA, true)) {
 					String root = getProductPath().toString();
 					if (PDECore.getDefault().getModelManager().isOSGiRuntime())
 						root += "/configuration"; //$NON-NLS-1$
 					fConfigDir = new File(root);
 				} else {
-					fConfigDir = LauncherUtils.createConfigArea(config.getName());
+					fConfigDir = LauncherUtils.createConfigArea(config);
 				}
 			} catch (CoreException e) {
-				fConfigDir = LauncherUtils.createConfigArea(config.getName());
+				fConfigDir = LauncherUtils.createConfigArea(config);
 			}
 		}
 		if (!fConfigDir.exists())
