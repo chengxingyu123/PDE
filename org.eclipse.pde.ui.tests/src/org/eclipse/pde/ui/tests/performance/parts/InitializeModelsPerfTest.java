8f1fdf5706d16b31e4c2ed78b86e8801c17119fc
wmelhem <wmelhem>
2005-04-02 06:28:48 +0000

    *** empty log message ***

---
+++ org.eclipse.pde.ui.tests/src/org/eclipse/pde/ui/tests/performance/parts/InitializeModelsPerfTest.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.pde.ui.tests.performance.parts;
+
+import java.io.File;
+import java.net.URL;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.pde.internal.core.ExternalModelManager;
+import org.eclipse.pde.internal.core.PDECore;
+import org.eclipse.pde.internal.core.PDEState;
+import org.eclipse.pde.internal.core.PluginPathFinder;
+import org.eclipse.pde.internal.core.TargetPlatformRegistryLoader;
+import org.eclipse.test.performance.Dimension;
+import org.eclipse.test.performance.PerformanceTestCase;
+
+public class InitializeModelsPerfTest extends PerformanceTestCase {
+
+	public static Test suite() {
+		return new TestSuite(InitializeModelsPerfTest.class);
+	}
+	
+	public void testModels() throws Exception {
+		tagAsSummary("Initialize Plug-ins (no caching)", Dimension.CPU_TIME);
+		String path = ExternalModelManager.getEclipseHome().toOSString();
+		startMeasuring();
+		createState();
+		stopMeasuring();
+		commitMeasurements();
+		assertPerformance();
+	}
+	
+	private void createState() {
+		PDEState state = new PDEState();
+		URL[] pluginPaths = PluginPathFinder.getPluginPaths(ExternalModelManager.getEclipseHome().toOSString());
+		TargetPlatformRegistryLoader.loadModels(pluginPaths, true, state, new NullProgressMonitor());				
+	}
+	
+	public void testCachedModels() throws Exception {
+		tagAsGlobalSummary("Initialize Plug-ins (with caching)", Dimension.CPU_TIME);
+		String path = ExternalModelManager.getEclipseHome().toOSString();
+		startMeasuring();
+		createState();
+		stopMeasuring();
+		commitMeasurements();
+		assertPerformance();
+	}
+	
+	protected void tearDown() throws Exception {
+		deleteContent(new File(PDECore.getDefault().getStateLocation().toOSString()));
+	}
+	
+	private void deleteContent(File curr) {
+		if (curr.exists()) {
+			if (curr.isDirectory()) {
+				File[] children = curr.listFiles();
+				if (children != null) {
+					for (int i = 0; i < children.length; i++) {
+						deleteContent(children[i]);
+					}
+				}
+			}
+			curr.delete();
+		}
+	}
+
+	
+}
