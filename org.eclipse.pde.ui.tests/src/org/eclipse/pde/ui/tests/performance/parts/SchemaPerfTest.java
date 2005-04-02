93fcb772839c59e454f83351bc1d3a0e48dff1c3
wmelhem <wmelhem>
2005-04-02 06:36:16 +0000

    *** empty log message ***

---
+++ org.eclipse.pde.ui.tests/src/org/eclipse/pde/ui/tests/performance/parts/SchemaPerfTest.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.pde.ui.tests.performance.parts;
+
+import junit.framework.*;
+
+import org.eclipse.pde.core.plugin.IPluginExtensionPoint;
+import org.eclipse.pde.core.plugin.IPluginModelBase;
+import org.eclipse.pde.internal.core.ModelEntry;
+import org.eclipse.pde.internal.core.PDECore;
+import org.eclipse.test.performance.*;
+
+public class SchemaPerfTest extends PerformanceTestCase {
+
+	public static Test suite() {
+		return new TestSuite(SchemaPerfTest.class);
+	}
+	
+	public void testLoadAllSchemas() throws Exception {
+		tagAsSummary("Loading all schemas", Dimension.USED_JAVA_HEAP);
+		ModelEntry[] entries = PDECore.getDefault().getModelManager().getEntries();
+		startMeasuring();
+		for (int i = 0; i < entries.length; i++) {
+			IPluginModelBase model = entries[i].getActiveModel();
+			if (model == null)
+				continue;
+			IPluginExtensionPoint[] extPoints = model.getPluginBase().getExtensionPoints();
+			for (int j = 0; j < extPoints.length; j++) {
+				PDECore.getDefault().getSchemaRegistry().getSchema(extPoints[j].getFullId());
+			}
+		}
+		stopMeasuring();
+		commitMeasurements();
+		assertPerformance();
+	}
+	
+}
